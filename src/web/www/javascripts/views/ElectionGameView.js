026b6d35953c94ac48eb4c6486acef94d69ac25b
Benny Daon <bennydaon@gmail.com>
2012-12-18 11:13:35 +0200

    Intial game screen layout

--- src/web/www/javascripts/views/ElectionGameView.js
+++ src/web/www/javascripts/views/ElectionGameView.js
@@ -1,12 +1,47 @@
 /**
  * The Election Game!
  */
-OKnesset.app.views.ElectionGameView = new Ext.extend(Ext.List, {
+var lists = {
+    xtype: 'list',
     id: 'ElectionGameView',
     store: OKnesset.PartyStore,
     grouped: true,
     itemTpl: '<div class="partyName">{name}<div class="partySize">{number_of_seats}</div></div>',
-    onItemDisclosure: true
-});
+    onItemDisclosure: true,
+    flex: 1
+};
+var grass = {
+  height: "100%",
+   xtype: "panel",
+   style: "background-color:green;",
+   html: "Green Grass",
+   flex: 4
+};
 
+var raceTrack = {
+  xtype: 'panel',
+  layout:  'hbox',
+  items: [lists, grass],
+  flex: 5
+}
+
+var question = {
+   layout: "fit",
+   flex: 1,
+   style: "background-color:yellow",
+   html: "why, why, why?",
+};
+
+var controls = {
+   style: "background-color:black",
+   html: "slider, next & back",
+   docked: "bottom"
+};
+
+
+OKnesset.app.views.ElectionGameView = new Ext.extend(Ext.Container, {
+  fullscreen: true,
+  layout: 'vbox',
+  items: [raceTrack, question, controls]
+});
 Ext.reg('ElectionGameView', OKnesset.app.views.ElectionGameView);
